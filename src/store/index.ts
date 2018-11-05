d0f135c1ea9f844affff2938865e44c66d007b86
Brian Boyko <brian.boyko@gmail.com>
2018-11-05 18:28:56 +0000

    added .env.development file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import BusRoutes from '@/store/modules/busRoutes';
-import BusStops from '@/store/modules/busStops';
-import BusTimetable from '@/store/modules/busTimetable';
-import Api from '@/apiInterface';
+import BusRoutes from "@/store/modules/busRoutes";
+import BusStops from "@/store/modules/busStops";
+import BusTimetable from "@/store/modules/busTimetable";
+import Api from "@/apiInterface";
 
-import createLogger from 'vuex/dist/logger'
+import createLogger from "vuex/dist/logger";
 
 const api = new Api();
 Vue.use(Vuex);
 
+console.log(process.env.NODE_ENV);
+
 export default new Vuex.Store({
-  plugins: [createLogger()],
+  plugins: process.env.NODE_ENV === "development" 
+    ? [createLogger({})] 
+    : [],
   state: {},
   mutations: {},
   actions: {},
   modules: {
     BusRoutes: BusRoutes(api),
     BusStops: BusStops(api),
-    BusTimetable: BusTimetable(api),
-  },
+    BusTimetable: BusTimetable(api)
+  }
 });
-
